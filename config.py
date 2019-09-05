a0e64f1e5da59df07449a62194f67c7645c1d4d1
IlyaFaer <ilya.faer@mail.ru>
2019-09-05 16:56:50 +0300

    Add Datastore label

--- config.py
+++ config.py
@@ -3,166 +3,125 @@ File that describes spreadsheet's architecture, including
 columns style, data validation, tracked repositories,
 tracked fields, etc..
 """
-from const import GREY, GREEN, BLUE, PINK, \
-    RED_KRAYOLA, GREEN_GOOD, WHITE, YELLOW
+from const import GREY, GREEN, BLUE, PINK, RED_KRAYOLA, GREEN_GOOD, WHITE, YELLOW
 
 
-TITLE = 'QLogic Internal Review'  # spreadsheet title
+TITLE = "QLogic Internal Review"  # spreadsheet title
 
 # sheets structure
 SHEETS = {
     # -----------------------------
-    'Python': {  # sheet name
-        'labels': {  # meaningful labels
-            'api: storage': 'Storage',
-            'api: firestore': 'FireStore',
-            'api: bigquery': 'BigQuery',
-            'api: bigtable': 'BigTable',
-            'api: spanner': 'Spanner',
-            'api: pubsub': 'PubSub',
-            'api: core': 'Core',
+    "Python": {  # sheet name
+        "labels": {  # meaningful labels
+            "api: storage": "Storage",
+            "api: firestore": "FireStore",
+            "api: bigquery": "BigQuery",
+            "api: bigtable": "BigTable",
+            "api: spanner": "Spanner",
+            "api: pubsub": "PubSub",
+            "api: core": "Core",
+            "api: datastore": "Datastore",
         },
-        'repo_names': {  # repos to track
-            'googleapis/google-cloud-python': 'GCP',
-            'googleapis/google-resumable-media-python': 'GRMP',
-            'q-logic/google-cloud-python': 'Q-GCP',
-            'q-logic/google-resumable-media-python': 'Q-GRMP',
+        "repo_names": {  # repos to track
+            "googleapis/google-cloud-python": "GCP",
+            "googleapis/google-resumable-media-python": "GRMP",
+            "q-logic/google-cloud-python": "Q-GCP",
+            "q-logic/google-resumable-media-python": "Q-GRMP",
         },
-        'team': {  # team, that works in this repos
-            'Ilya': 'IlyaFaer',
-            'Hemang': 'HemangChothani',
-            'Maxim': 'mf2199',
-            'Paras': 'pchauhan-qlogic',
-            'Sumit': 'sumit-ql',
-            'Sangram': 'sangramql',
-            'Leonid': 'Emar-Kar',
-            'N/A': '',
-            'Other': 'Other',
-        }
+        "team": {  # team, that works in this repos
+            "Ilya": "IlyaFaer",
+            "Hemang": "HemangChothani",
+            "Maxim": "mf2199",
+            "Paras": "pchauhan-qlogic",
+            "Sumit": "sumit-ql",
+            "Sangram": "sangramql",
+            "Leonid": "Emar-Kar",
+            "N/A": "",
+            "Other": "Other",
+        },
     },
     # -----------------------------
-    'NodeJS': {
-        'labels': {},
-        'repo_names': {
-            'googleapis/nodejs-storage': 'Storage',
-            'googleapis/nodejs-firestore': 'FireStore',
-            'googleapis/nodejs-bigquery': 'BigQuery',
-            'googleapis/nodejs-spanner': 'Spanner',
-            'googleapis/nodejs-bigtable': 'BigTable',
-            'googleapis/nodejs-pubsub': 'PubSub',
+    "NodeJS": {
+        "labels": {},
+        "repo_names": {
+            "googleapis/nodejs-storage": "Storage",
+            "googleapis/nodejs-firestore": "FireStore",
+            "googleapis/nodejs-bigquery": "BigQuery",
+            "googleapis/nodejs-spanner": "Spanner",
+            "googleapis/nodejs-bigtable": "BigTable",
+            "googleapis/nodejs-pubsub": "PubSub",
         },
-        'team': {
-            'Lalji': 'laljikanjareeya',
-            'Praveen': 'praveenqlogic01',
-            'Jiren': 'jiren',
-            'Vishal': 'vishald123',
-            'Alex': 'AVaksman',
-            'Ivan': 'IvanAvanessov',
-            'N/A': '',
-            'Other': 'Other',
-        }
+        "team": {
+            "Lalji": "laljikanjareeya",
+            "Praveen": "praveenqlogic01",
+            "Jiren": "jiren",
+            "Vishal": "vishald123",
+            "Alex": "AVaksman",
+            "Ivan": "IvanAvanessov",
+            "N/A": "",
+            "Other": "Other",
+        },
     },
     # -----------------------------
-    'Golang': {
-        'labels': {
-            'api: storage': 'Storage',
-            'api: firestore': 'FireStore',
-            'api: bigquery': 'BigQuery',
-            'api: bigtable': 'BigTable',
-            'api: spanner': 'Spanner',
-            'api: pubsub': 'PubSub',
+    "Golang": {
+        "labels": {
+            "api: storage": "Storage",
+            "api: firestore": "FireStore",
+            "api: bigquery": "BigQuery",
+            "api: bigtable": "BigTable",
+            "api: spanner": "Spanner",
+            "api: pubsub": "PubSub",
         },
-        'repo_names': {
-            'googleapis/google-cloud-go': 'GCG',
+        "repo_names": {"googleapis/google-cloud-go": "GCG"},
+        "team": {
+            "Ilya": "IlyaFaer",
+            "Aleksandra": "AlesskaPie",
+            "Emmanuel": "odeke-em",
+            "N/A": "",
+            "Other": "Other",
         },
-        'team': {
-            'Ilya': 'IlyaFaer',
-            'Aleksandra': 'AlesskaPie',
-            'Emmanuel': 'odeke-em',
-            'N/A': '',
-            'Other': 'Other',
-        }
-    }
+    },
 }
 
 # fields, that must be updated on every sheet update
 # other fields will be left unchanged
-TRACKED_FIELDS = ('Created', 'Description', 'Project', 'Assignee')
+TRACKED_FIELDS = ("Created", "Description", "Project", "Assignee")
 
 # columns structure
 COLUMNS = (
     {
-        'name': 'Priority',  # 0
-        'width': 80,
-        'align': 'CENTER',
-        'values': {  # possible value with it's color
-            'Closed': GREY,
-            'Low': GREEN,
-            'Medium': BLUE,
-            'High': PINK,
-            'Critical': RED_KRAYOLA,
-            'Done': GREEN_GOOD,
-            'New': WHITE,
-        }
+        "name": "Priority",  # 0
+        "width": 80,
+        "align": "CENTER",
+        "values": {  # possible value with it's color
+            "Closed": GREY,
+            "Low": GREEN,
+            "Medium": BLUE,
+            "High": PINK,
+            "Critical": RED_KRAYOLA,
+            "Done": GREEN_GOOD,
+            "New": WHITE,
+        },
     },
+    {"name": "Issue", "width": 50, "align": "CENTER", "type": "link"},  # 1
     {
-        'name': 'Issue',  # 1
-        'width': 50,
-        'align': 'CENTER',
-        'type': 'link',
+        "name": "Work status",  # 2
+        "align": "CENTER",
+        "values": {
+            "Pending": WHITE,
+            "In progress": GREEN,
+            "Paused": YELLOW,
+            "Finished": GREY,
+        },
     },
-    {
-        'name': 'Work status',  # 2
-        'align': 'CENTER',
-        'values': {
-            'Pending': WHITE,
-            'In progress': GREEN,
-            'Paused': YELLOW,
-            'Finished': GREY,
-        }
-    },
-    {
-        'name': 'Created',  # 3
-        'align': 'CENTER',
-        'type': 'date'
-    },
-    {
-        'name': 'Description',  # 4
-        'width': 450
-    },
-    {
-        'name': 'Repository',  # 5
-        'align': 'CENTER'
-    },
-    {
-        'name': 'Project',  # 6
-        'align': 'CENTER'
-    },
-    {
-        'name': 'Assignee',  # 7
-        'align': 'CENTER',
-    },
-    {
-        'name': 'Internal PR',  # 8
-        'align': 'CENTER',
-        'type': 'link',
-    },
-    {
-        'name': 'Public PR',  # 9
-        'align': 'CENTER',
-        'type': 'link'
-    },
-    {
-        'name': 'Task',  # 10
-        'align': 'CENTER'
-    },
-    {
-        'name': 'Opened',  # 11
-        'align': 'CENTER',
-        'type': 'date'
-    },
-    {
-        'name': 'Comment',  # 12
-        'width': 550
-    },
+    {"name": "Created", "align": "CENTER", "type": "date"},  # 3
+    {"name": "Description", "width": 450},  # 4
+    {"name": "Repository", "align": "CENTER"},  # 5
+    {"name": "Project", "align": "CENTER"},  # 6
+    {"name": "Assignee", "align": "CENTER"},  # 7
+    {"name": "Internal PR", "align": "CENTER", "type": "link"},  # 8
+    {"name": "Public PR", "align": "CENTER", "type": "link"},  # 9
+    {"name": "Task", "align": "CENTER"},  # 10
+    {"name": "Opened", "align": "CENTER", "type": "date"},  # 11
+    {"name": "Comment", "width": 550},  # 12
 )
