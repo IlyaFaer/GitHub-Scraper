b4411370ad6bdabb519568afcbc4ff9eea33a804
IlyaFaer <ilya.faer@mail.ru>
2019-09-30 10:29:09 +0300

    Backend issues will got low priority.

--- fill_funcs.py
+++ fill_funcs.py
@@ -25,31 +25,36 @@ def fill_priority(old_issue, issue):
     date_diff = datetime.date.today() - issue.created_at.date()
     # if issue have been new for three or more days,
     # auto designate it's priority
-    if old_issue["Priority"] == "New" and date_diff.days > 3:
-        our_labels = []
-        labels = [label.name for label in issue.labels]
+    labels = [label.name for label in issue.labels]
 
-        for our_label in (
-            "api: storage",
-            "api: spanner",
-            "api: firestore",
-            "api: datastore",
-            "api: bigtable",
-            "api: pubsub",
-            "api: core",
-        ):
-            if our_label in labels:
-                our_labels.append(our_label)
+    if old_issue["Priority"] == "New":
+        if date_diff.days > 3:
+            our_labels = []
 
-        if our_labels:
-            # bugs in our projects have high priority
-            if "type: bug" in labels:
-                old_issue["Priority"] = "High"
-            # other issues
+            for our_label in (
+                "api: storage",
+                "api: spanner",
+                "api: firestore",
+                "api: datastore",
+                "api: bigtable",
+                "api: pubsub",
+                "api: core",
+            ):
+                if our_label in labels:
+                    our_labels.append(our_label)
+
+            if our_labels:
+                # bugs in our projects have high priority
+                if "type: bug" in labels:
+                    old_issue["Priority"] = "High"
+                # other issues
+                else:
+                    old_issue["Priority"] = "Medium"
+            # other projects
             else:
-                old_issue["Priority"] = "Medium"
-        # other projects
-        else:
+                old_issue["Priority"] = "Low"
+    else:
+        if "backend" in labels:
             old_issue["Priority"] = "Low"
 
 
