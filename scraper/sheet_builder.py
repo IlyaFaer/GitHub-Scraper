389f27ed939c6e4d31c177918fcccfd2a01a2124
IlyaFaer <ilya.faer@mail.ru>
2020-03-02 17:06:56 +0300

    Revert deleting updated_issues dict.

--- scraper/sheet_builder.py
+++ scraper/sheet_builder.py
@@ -44,6 +44,7 @@ class SheetBuilder:
                 {issue.html_url: github.Issue.Issue}
         """
         is_first_update = False
+        updated_issues = {}
 
         for repo_name in self._repo_names.keys():
             repo = self._repos.setdefault(
@@ -60,7 +61,7 @@ class SheetBuilder:
                     self._last_issue_updates[repo_name] = datetime.datetime(1, 1, 1)
                     is_first_update = True
 
-                self._process_issue(issue)
+                self._process_issue(issue, updated_issues)
 
                 self._last_issue_updates[repo_name] = max(
                     self._last_issue_updates[repo_name], issue.updated_at
@@ -75,7 +76,8 @@ class SheetBuilder:
                         )
             logging.info("{repo}: issues processed".format(repo=repo.full_name))
 
-        return self._issues_index
+        self._issues_index.update(updated_issues)
+        return updated_issues
 
     def get_from_index(self, tracked_id):
         """Get issue object saved in internal index.
@@ -165,14 +167,15 @@ class SheetBuilder:
 
         return Github(login, password)
 
-    def _process_issue(self, issue):
+    def _process_issue(self, issue, updated_issues):
         """If issue is PR, indexate it. Add into updated index otherwise.
 
         Args:
             issue (github.Issue.Issue): Issue object.
+            updated_issues (dict): Updated issues index.
         """
         if issue.pull_request is None:
-            self._issues_index[issue.html_url] = issue
+            updated_issues[issue.html_url] = issue
             return
 
         # issue is pull request - indexate it
